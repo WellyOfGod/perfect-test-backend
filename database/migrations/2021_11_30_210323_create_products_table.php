fd93eb9d8a2794c8bce736cb856c59aaba7667b2
Wellington Lopes <wellyofgod@gmail.com>
2021-11-30 18:11:09 -0300

    Parameterizing products table

--- database/migrations/2021_11_30_210323_create_products_table.php
+++ database/migrations/2021_11_30_210323_create_products_table.php
@@ -11,10 +11,14 @@ class CreateProductsTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 256);
+            $table->text('description', 1024);
+            $table->unsignedDecimal('price', 10, 2);
+            $table->string('photo_path')->nullable();
             $table->timestamps();
         });
     }
@@ -24,7 +28,7 @@ class CreateProductsTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('products');
     }

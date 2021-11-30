4d8424da10f77b8692af393ce7c910788d607df0
Wellington Lopes <wellyofgod@gmail.com>
2021-11-30 18:16:24 -0300

    Parameterizing sales table and foreign

---
+++ database/migrations/2021_11_30_210551_create_sales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('quantity');
+            $table->unsignedDecimal('discount')->nullable();
+            $table->unsignedDecimal('total');
+            $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('sale_situation_id')->constrained('sale_situations');
+            $table->foreignId('customers_id')->constrained('customers');
+            $table->datetime('dh_sold')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+}

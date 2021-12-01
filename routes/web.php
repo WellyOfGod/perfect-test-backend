6dc40d7266edcf9f6cd56daee9cdd186eb80fc23
Wellington Lopes <wellyofgod@gmail.com>
2021-12-01 14:47:40 -0300

    Removing unnecessary lines

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 
-//Route::match([Request::METHOD_GET, Request::METHOD_POST], '/', 'DashboardController')->name('dashboard');
-
-//Route::resources([
-//    'product' => 'ProductController',
-//    'client'  => 'ClientController',
-//    'sale'    => 'SaleController',
-//], ['except' => ['index', 'show']]);
-
 Route::resource('product', 'ProductController');
 Route::resource('sale', 'SaleController');
 

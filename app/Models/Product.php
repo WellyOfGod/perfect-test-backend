4aa867de09d4546384796b4c2b7da179bda2850f
Wellington Lopes <wellyofgod@gmail.com>
2021-12-01 12:17:46 -0300

    Creating Model Product

---
+++ app/Models/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static create($validated)
+ */
+class Product extends Model
+{
+    protected $guarded = ['id'];
+}

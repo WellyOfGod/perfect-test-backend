6e74e49b77a2d348661aec69e806aeee4670169a
Wellington Lopes <wellyofgod@gmail.com>
2021-12-01 15:33:47 -0300

    Rename namespace

--- app/Http/Requests/Product/StoreRequest.php
+++ app/Http/Requests/Product/StoreRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Product;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ProductUpdateRequest extends FormRequest
+class StoreRequest extends FormRequest
 {
 
 
--- app/Http/Requests/Product/UpdateRequest.php
+++ app/Http/Requests/Product/UpdateRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Product;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ProductStoreRequest extends FormRequest
+class UpdateRequest extends FormRequest
 {
 
 

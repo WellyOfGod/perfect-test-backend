30c9b2798f8e08ec9dda2e85cbfec1ca7112d4b4
Wellington Lopes <wellyofgod@gmail.com>
2021-12-01 12:20:34 -0300

    Parameterizing crud_product frontend with backend

--- resources/views/crud_products.blade.php
+++ resources/views/crud_products.blade.php
@@ -4,18 +4,46 @@
     <h1>Adicionar / Editar Produto</h1>
     <div class='card'>
         <div class='card-body'>
-            <form>
+            <form action="{{ route('product.store')}}" method="post" enctype="multipart/form-data">
+                @if(request()->routeIs('product.edit'))
+                    @method('PUT')
+                @endif
+                @csrf
                 <div class="form-group">
                     <label for="name">Nome do produto</label>
-                    <input type="text" class="form-control " id="name">
+                    <input type="text" name="name"
+                           class="form-control @error('name') is-invalid @enderror"
+                           id="name" value="{{old('name') ?? $product->name ?? ''}}">
+                    @error('name')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="description">Descrição</label>
-                    <textarea type="text" rows='5' class="form-control" id="description"></textarea>
+                    <textarea type="text" name="description" rows='5'
+                              class="form-control @error('description') is-invalid @enderror"
+                              id="description">{{old('description') ?? $product->description ?? ''}}
+                    </textarea>
+                    @error('description')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <div class="form-group">
                     <label for="price">Preço</label>
-                    <input type="text" class="form-control" id="price" placeholder="100,00 ou maior">
+                    <input type="number" step="0.01"  name="price"
+                           class="form-control @error('price') is-invalid @enderror"
+                           id="price" placeholder="Digite o preço do produto"
+                           value="{{old('price') ?? $product->price ?? ''}}">
+                    @error('price')
+
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
                 <button type="submit" class="btn btn-primary">Salvar</button>
             </form>

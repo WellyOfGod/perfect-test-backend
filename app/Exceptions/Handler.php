80f6aa8a418c24bc8b171443b5ebd2ca77cfb040
Wellington Lopes <wellyofgod@gmail.com>
2021-12-01 22:07:12 -0300

    Refactoring Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,7 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Auth\{AuthenticationException, Access\AuthorizationException};
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\Exceptions\ThrottleRequestsException;
-use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,21 +22,21 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontFlash = [
-        'current_password',
         'password',
         'password_confirmation',
     ];
 
     /**
-     * Register the exception handling callbacks for the application.
+     * Report or log an exception.
      *
+     * @param  \Throwable  $exception
      * @return void
+     *
+     * @throws \Exception
      */
-    public function register()
+    public function report(Throwable $exception)
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        parent::report($exception);
     }
 
     /**
@@ -48,37 +44,12 @@ class Handler extends ExceptionHandler
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Throwable  $exception
-     * @return \Symfony\Component\HttpFoundation\Response |
-     * \Illuminate\Database\Eloquent\ModelNotFoundException |
-     * \Illuminate\Validation\ValidationException |
-     * \Illuminate\Auth\Access\AuthorizationException |
-     * \Illuminate\Auth\AuthenticationException |
-     * \Illuminate\Http\Exceptions\ThrottleRequestsException
+     * @return \Symfony\Component\HttpFoundation\Response
      *
      * @throws \Throwable
      */
     public function render($request, Throwable $exception)
     {
-        if ($exception instanceof ModelNotFoundException) {
-            return response()->json(['message' => 'Registro não encontrado.'], 404);
-        }
-
-        if ($exception instanceof ValidationException) {
-            return response()->json(['message' => 'Dados informado(s) inválido(s).', 'errors' => $exception->validator->getMessageBag()], 422);
-        }
-
-        if ($exception instanceof AuthorizationException) {
-            return response()->json(['message' => 'Autorização não concedida.'], 403);
-        }
-
-        if ($exception instanceof AuthenticationException) {
-            return response()->json(['message' => 'Você não está autenticado.'], 401);
-        }
-
-        if ($exception instanceof ThrottleRequestsException) {
-            return response()->json(['message' => 'Muitas requisições, tente mais tarde.'], 429);
-        }
-
         return parent::render($request, $exception);
     }
 }

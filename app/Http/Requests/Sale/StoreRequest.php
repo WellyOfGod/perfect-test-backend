bbe20cfd0ceb822f82653e3afc54f2b2fdedffcd
Wellington Lopes <wellyofgod@gmail.com>
2021-12-02 12:37:56 -0300

    Parameterizing Forms Requests Sale

--- app/Http/Requests/Sale/StoreRequest.php
+++ app/Http/Requests/Sale/StoreRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Requests\Sale;
 
+use App\Rules\CPF;
 use Illuminate\Foundation\Http\FormRequest;
 use Carbon\Carbon;
+use App\Models\{Customer, Product, SaleSituation};
 
 class StoreRequest extends FormRequest
 {
@@ -30,20 +32,21 @@ class StoreRequest extends FormRequest
      */
     public function rules(): array
     {
+
         if ($this->customer_id) {
-            $rules['customer_id'] = 'required|integer|exists:customer,id';
+            $rules['customer_id'] = 'required|integer|exists:' .Customer::class.',id';
         } else {
             $rules['name']  = 'required|string|between:1,256';
             $rules['email'] = 'required|email|between:1,256';
-            $rules['cpf']   = 'required|cpf|formato_cpf';
+            $rules['cpf']   = ['required', 'string', 'min:11', 'max:14','unique:' .Customer::class.',cpf,{$id}', new CPF];
         }
 
         return $rules + [
-                'product_id'        => 'required|integer|exists:products,id',
+                'product_id'        => 'required|integer|exists:' .Product::class.',id',
                 'sold_at'           => 'required|date|date_format:Y-m-d',
                 'quantity'          => 'required|integer|between:1,999999999',
                 'discount'          => 'required|numeric|between:0,100.00',
-                'sale_situation_id' => 'required|integer|exists:sale_situations,id'
+                'sale_situation_id' => 'required|integer|exists:' .SaleSituation::class.',id'
             ];
     }
 }
--- app/Http/Requests/Sale/UpdateRequest.php
+++ app/Http/Requests/Sale/UpdateRequest.php
@@ -2,20 +2,21 @@
 
 namespace App\Http\Requests\Sale;
 
+use App\Rules\CPF;
 use Illuminate\Foundation\Http\FormRequest;
 use Carbon\Carbon;
+use App\Models\{Customer, Product, SaleSituation};
 
-class UpdateRequest extends FormRequest
+class StoreRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
      * @return bool
      */
     public function authorize(): bool
     {
         return true;
     }
+
 
     protected function prepareForValidation(): void
     {
@@ -27,19 +28,25 @@ class UpdateRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
      * @return array
      */
     public function rules(): array
     {
-        return [
-            'customer_id'         => 'required|integer|exists:customer,id',
-            'product_id'        => 'required|integer|exists:products,id',
-            'sold_at'           => 'required|date|date_format:Y-m-d',
-            'quantity'          => 'required|integer|between:1,999999999',
-            'discount'          => 'required|numeric|between:0,100.00',
-            'sale_situation_id' => 'required|integer|exists:sale_situations,id'
-        ];
+
+        if ($this->customer_id) {
+            $rules['customer_id'] = 'required|integer|exists:' .Customer::class.',id';
+        } else {
+            $rules['name']  = 'required|string|between:1,256';
+            $rules['email'] = 'required|email|between:1,256';
+            $rules['cpf']   = ['required', 'string', 'min:11', 'max:14','unique:' .Customer::class.',cpf,{$id}', new CPF];
+        }
+
+        return $rules + [
+                'product_id'        => 'required|integer|exists:' .Product::class.',id',
+                'sold_at'           => 'required|date|date_format:Y-m-d',
+                'quantity'          => 'required|integer|between:1,999999999',
+                'discount'          => 'required|numeric|between:0,100.00',
+                'sale_situation_id' => 'required|integer|exists:' .SaleSituation::class.',id'
+            ];
     }
 }

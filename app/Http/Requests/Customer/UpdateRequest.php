22e64d997a0031069672dd8be01f309a57931405
Wellington Lopes <wellyofgod@gmail.com>
2021-12-01 22:12:20 -0300

    Refactoring UpdateRequest Customer

--- app/Http/Requests/Customer/UpdateRequest.php
+++ app/Http/Requests/Customer/UpdateRequest.php
@@ -5,11 +5,9 @@ namespace App\Http\Requests\Customer;
 use App\Rules\CPF;
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateRequest extends FormRequest
+class StoreRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
      * @return bool
      */
     public function authorize(): bool
@@ -18,16 +16,16 @@ class UpdateRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
      * @return array
      */
     public function rules(): array
     {
+        $id = $this->customer ? ",{$this->customer->id}" : '';
+
         return [
-            'name'  => 'required|string',
-            'email' => 'required|email:rfc,dns',
-            'cpf'   => ['required', 'string', 'min:11', 'max:14', new CPF]
+            'name'  => 'required|string|max:64',
+            'email'  => "nullable|string|max:128|unique:App\Models\Customers,email{$id}",
+            'cpf'   => ['required', 'string', 'min:11', 'max:14','unique:App\Models\Customers,cpf,{$id}', new CPF]
         ];
     }
 }
